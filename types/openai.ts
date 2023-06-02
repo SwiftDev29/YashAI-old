55e444efa0eebb5a5d9f81aa4a4ce1d3b40801b8
YashSinha2006 <63844958+YashSinha2006@users.noreply.github.com>
2023-06-02 19:25:54 +0530

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -32,7 +32,7 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
-    name: 'YashAI',
+    name: 'YashAI v1.5 powered by GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
   },
