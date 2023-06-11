940b091ea38e658374fb22a64f35e977229f807a
YashSinha2006 <63844958+YashSinha2006@users.noreply.github.com>
2023-06-11 14:52:19 +0530

    Update LoginForm.tsx

--- components/Settings/LoginForm.tsx
+++ components/Settings/LoginForm.tsx
@@ -12,81 +12,45 @@ type Props = {
 export default function LoginForm({ onLogin, username, password }: Props) {
   const [isLoading, setIsLoading] = useState(false);
 
-  const bypassAuth = true; // Set to true to enable bypass, set to false to disable bypass
-
-  const handleSubmit = async (values: { username: string; password: string }) => {
-    setIsLoading(true);
-
-    if (
-      bypassAuth &&
-      values.username.trim() === '' &&
-      values.password.trim() === ''
-    ) {
-      console.log('Bypassing authentication');
-      Cookies.set('isLoggedIn', 'true', { expires: 1 });
-      onLogin();
-      setIsLoading(false);
-      return;
-    }
-
-    // Check if the entered username and password match the ones from the .env file
-    if (values.username === username && values.password === password) {
-      console.log('Credentials match.');
-      Cookies.set('isLoggedIn', 'true', { expires: 1 }); // Set a cookie for 1 day
-      onLogin();
-    } else {
-      console.log('Credentials do not match.');
-      // If the authentication fails, show an error message
-      toast.error('Invalid username or password.');
-    }
-
-    setIsLoading(false);
-  };
+  // ... (rest of the code)
 
   return (
-        
-        <div className="flex items-center justify-center min-h-screen bg-gray-50">
-            <Formik
-                initialValues={{
-                    username: '',
-                    password: '',
-                }}
-                onSubmit={handleSubmit}
-            >
-                <Form className="bg-white p-8 rounded-md shadow-md">
-                    <h1 className="text-2xl font-medium text-gray-800 mb-4">Login</h1>
-                    <div className="mb-4">
-                        <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
-                            Username
-                        </label>
-                        <Field
-                            id="username"
-                            name="username"
-                            placeholder="Username"
-                            className="border border-gray-300 p-2 rounded-md w-full"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
-                            Password
-                        </label>
-                        <Field
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Password"
-                            className="border border-gray-300 p-2 rounded-md w-full"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 w-full"
-                        disabled={isLoading}
-                    >
-                        {isLoading ? 'Logging in...' : 'Login'}
-                    </button>
-                </Form>
-            </Formik>
-        </div>
-    );
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <Formik initialValues={{ username: '', password: '' }} onSubmit={handleSubmit}>
+        <Form className="bg-gray-700 p-8 rounded-md shadow-md">
+          <h1 className="text-2xl font-medium text-white mb-4">Login</h1>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-300 font-medium mb-2">
+              Username
+            </label>
+            <Field
+              id="username"
+              name="username"
+              placeholder="Username"
+              className="border border-gray-600 p-2 rounded-md w-full bg-gray-800 text-white"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-300 font-medium mb-2">
+              Password
+            </label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className="border border-gray-600 p-2 rounded-md w-full bg-gray-800 text-white"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 w-full"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Logging in...' : 'Login'}
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
