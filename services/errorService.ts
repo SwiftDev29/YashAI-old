5309b284168fd4def490c5cf8b784bbb165846f0
YashSinha2006 <63844958+YashSinha2006@users.noreply.github.com>
2023-06-13 10:11:14 +0530

    Update errorService.ts

--- services/errorService.ts
+++ services/errorService.ts
@@ -10,24 +10,9 @@ const useErrorService = () => {
   return {
     getModelsError: useMemo(
       () => (error: any) => {
-        return !error
-          ? null
-          : ({
-              title: t('Error fetching models.'),
-              code: error.status || 'unknown',
-              messageLines: error.statusText
-                ? [error.statusText]
-                : [
-                    t(
-                      'Make sure your OpenAI API key is set in the bottom left of the sidebar.',
-                    ),
-                    t(
-                      'If you completed this step, OpenAI may be experiencing issues.',
-                    ),
-                  ],
-            } as ErrorMessage);
+        return null; // Return null to remove the error
       },
-      [t],
+      [],
     ),
   };
 };
